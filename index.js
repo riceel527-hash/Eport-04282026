94ad749dc45c8104987863b252f07c3b32831080
riceel527-hash <riceel527@gmail.com>
2026-05-06 15:01:15 -0400

    added css

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-//template_s3chdi9
+//template_m74hasa
 //service_5k2ad7k
 //pcMbVjjCOaA8Ua1no
+
+function contact(event) {
+ event.preventDefault();
+    emailjs.sendForm(
+        'service_m74hasa', 
+        'template_s3chdi9', 
+        event.target
+}
+
