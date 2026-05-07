d28c7b7dcb98b5a02094506866aabdba13f1e65e
riceel527-hash <riceel527@gmail.com>
2026-05-07 13:25:43 -0400

    added css

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 
 function contact(event) {
  event.preventDefault();
-emailjs
-.sendForm(
+const loading = document.querySelector(".modal__overlay--loading");
+const success = document.querySelector(".modal__overlay--success");
+loading.classList += " modal__overlay--visible";
+emailjs.sendForm(
     'service_5k2ad7k',
     'template_m74hasa',
     event.target,
-    'pcMbVjjCOaA8Ua1no'
+    'pcMbVjjC0aA8Ua1no'
 ).then(() => {
-    console.log('this worked1')
-})
-
+    loading.classList.remove("modal__overlay--visible");
+    success.classList += " modal__overlay--visible";
+}).catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+   alert("The email service is temporarily unavailable. Please contact me directly on email@email.com"
+      );
+    })
 }
+
+//setTimeout(() => {
+
+//console.log('it worked 1')    
+//}, 1000);
+
+//}
+
